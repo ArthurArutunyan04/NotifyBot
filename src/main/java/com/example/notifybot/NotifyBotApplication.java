0281c526c22b7b58a0014cb7bb02bd34c7a45781
ArthurArutunyan04 <lartur2004@yandex.ru>
2025-03-26 17:27:52 +0300

    New start

---
+++ src/main/java/com/example/notifybot/NotifyBotApplication.java
@@ -0,0 +1,13 @@
+package com.example.notifybot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotifyBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotifyBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/notifybot/NotifyBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notifybot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotifyBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
