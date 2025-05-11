7134661f721f1370e035f5c077ba027ef9a3c071
ArthurArutunyan04 <lartur2004@yandex.ru>
2025-05-12 02:22:18 +0300

    T7

---
+++ auth-service/src/main/java/com/example/authservice/entity/TestEntity.java
@@ -0,0 +1,13 @@
+package com.example.authservice.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class TestEntity {
+    @Id
+    private Long id;
+    private String name;
+}
---
+++ auth-service/src/main/resources/db/migration/V1__init_test_table.sql
@@ -0,0 +1,4 @@
+CREATE TABLE test_entity (
+    id BIGINT PRIMARY KEY,
+    name VARCHAR(255)
+);
