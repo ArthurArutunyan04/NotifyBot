912868fb90509c88129d109b6d17a91c50a93da6
ArthurArutunyan04 <lartur2004@yandex.ru>
2025-05-23 12:53:55 +0300

    T101)

--- task-entrypoint/app.py
+++ task-entrypoint/app.py
@@ -16,7 +16,7 @@ def get_db_connection():
         dbname=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST
     )
 
-@app.route("/api/tasks", methods=["GET"])
+@app.route("/api/task", methods=["GET"])
 def get_tasks():
     try:
         conn = get_db_connection()
